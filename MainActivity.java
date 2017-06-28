31292733e6634700c5f1f449f8a78e6965b0793e
leoberus <ptrp.srirak@gmail.com>
2017-06-29 02:47:04 +0700

    Set Gender code and fill in Exercise Arrays

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,11 @@ package com.example.peen.workshop_bmr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    protected void onRadioButtonClicked(View view){
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        //Declare 2 button variable
+        Button male_btn = (Button)findViewById(R.id.radioButton);
+        Button female_btn = (Button)findViewById(R.id.radioButton2);
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButton:
+                if (checked)
+                    Toast.makeText(this, "Male", Toast.LENGTH_SHORT).show();
+                    break;
+            case R.id.radioButton2:
+                if (checked)
+                    Toast.makeText(this, "Female", Toast.LENGTH_SHORT).show();
+                    break;
+        }
+    }
+
 
 
 
