33a72a91a0cd1088e59a374c7ac5039ec6b78731
leoberus <ptrp.srirak@gmail.com>
2017-06-29 18:51:36 +0700

    uploade all function complete

--- MainActivity.java
+++ MainActivity.java
@@ -16,9 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
+
 
 
     protected void onRadioButtonClicked(View view){
